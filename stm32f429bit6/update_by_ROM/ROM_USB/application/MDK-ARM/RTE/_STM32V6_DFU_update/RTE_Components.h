83e7dd3b8fff1034f7fb8d7687a9baa9c450a885
shadow3d <shadowthreeD@gmail.com>
2024-06-05 17:34:03 +0800

    add stm32f429bit6 update by USB

---
+++ stm32f429bit6/update_by_ROM/ROM_USB/application/MDK-ARM/RTE/_STM32V6_DFU_update/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32V6_DFU_update' 
+ * Target:  'STM32V6_DFU_update' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
